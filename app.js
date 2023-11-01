356d69a72011a373c96ae5b528c21a5bb02e93fe
Carl Molina <carlandrew.molina@gmail.com>
2023-10-31 18:41:32 -0700

    Update w/ Bootstrap

--- app.js
+++ app.js
@@ -1,44 +1,55 @@
 const addMeme = document.querySelector('#add-meme');
 const imageUrl = document.querySelector('input[name="image-url"]');
 const textOnTop = document.querySelector('input[name="text-on-top"]');
+const textCenter = document.querySelector('input[name="text-center"]');
 const textOnBottom = document.querySelector('input[name="text-on-bottom"]');
 const memeArea = document.querySelector('#meme-area');
 
 addMeme.addEventListener('submit', function(e) {
   e.preventDefault();
-  const newMeme = memeCreator(imageUrl.value, textOnTop.value, textOnBottom.value);
+  const newMeme = memeCreator(imageUrl.value, textOnTop.value, textCenter.value, textOnBottom.value);
   memeArea.appendChild(newMeme);
   imageUrl.value = '';
   textOnTop.value = '';
+  textCenter.value = '';
   textOnBottom.value = '';
 });
 
 
-function memeCreator(url, top, bottom) {
+function memeCreator(url, top, center, bottom) {
   const memeContainer = document.createElement('div');
   const newImg = document.createElement('img');
   const topText = document.createElement('strong');
+  const centerText = document.createElement('strong');
   const bottomText = document.createElement('strong');
   const deleteMeme = document.createElement('h2');
 
   newImg.src = url;
   topText.innerText = top;
+  centerText.innerText = center;
   bottomText.innerText = bottom;
   deleteMeme.innerText = "X";
 
-  memeContainer.classList.add('container');
+  memeContainer.classList.add('completed-meme');
   newImg.classList.add('image');
-  topText.classList.add('text-top');
-  topText.classList.add('meme-text');
-  bottomText.classList.add('text-bottom');
-  bottomText.classList.add('meme-text');
+  topText.classList.add('text-top', 'meme-text');
+  centerText.classList.add('text-center', 'meme-text');
+  bottomText.classList.add('text-bottom', 'meme-text');
   deleteMeme.classList.add('hover-text');
 
 
   memeContainer.appendChild(newImg);
   memeContainer.appendChild(topText);
+  memeContainer.appendChild(centerText);
   memeContainer.appendChild(bottomText);
   memeContainer.appendChild(deleteMeme);
+
+  memeContainer.style.width = '33%';
+  memeContainer.style.height = '33%';
+
+  newImg.style.maxWidth = '100%';
+  newImg.style.maxHeight = '100%';
+  newImg.style.display = 'inline-block';
 
   return memeContainer;
 };
@@ -48,11 +59,3 @@ memeArea.addEventListener("click", function(e) {
     e.target.parentElement.remove();
   }
 });
-
-// memeArea.addEventListener("hover", function(e) {
-//   if (e.target.classList)
-// })
-
-//  memeArea.addEventListener("click", function(e) {
-//    console.log(e.target.tagName);
-//  });
